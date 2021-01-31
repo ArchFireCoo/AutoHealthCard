da043f37ce4f0b2d6fbecda5c99685c1c1e01d91
Jasonzj <925645402@qq.com>
2021-02-01 00:02:27 +0800

    Add support for specified date ranges

--- scripts/utils.js
+++ scripts/utils.js
@@ -20,12 +20,36 @@ const keysToLowerCase = (obj) => {
 
 const datePadZero = (item) => item.toString().padStart(2, '0')
 
+const getDaysBetween = (start, stop) => (Date.parse(stop) - Date.parse(start)) / (1 * 24 * 60 * 60 * 1000)
+
 const generateRangeArr = (start, stop, padStr) =>
-  Array.from({ length: stop - start + 1 }, (_, i) => padStr + (start + i))
+  Array.from({ length: stop - start + 1 }, (_, i) => padStr + (+start + i))
+
+const generateDateRangeArr = (start, stop) =>
+  Array.from({ length: getDaysBetween(start, stop) + 1 }, (_, i) => {
+    const date = new Date(start)
+    date.setDate(date.getDate() + i)
+    return date
+  })
+
+const createCurry = (func, args) => {
+  var arity = func.length
+  var args = args || []
+
+  return function () {
+    let _args = Array.from(arguments).concat(args)
+
+    if (_args.length < arity) return createCurry.call(this, func, _args)
+
+    return func.apply(this, _args)
+  }
+}
 
 module.exports = {
   removeNullsInObject,
   keysToLowerCase,
   datePadZero,
   generateRangeArr,
+  generateDateRangeArr,
+  createCurry,
 }
